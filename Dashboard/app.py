37028d0b544052f210712cd495c284d70026d162
Shinjiriki <shinjiriki21@gmail.com>
2021-04-13 02:05:21 +0700

    Add files via upload

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -35,35 +35,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/result', methods = ['POST','GET'])
-def result():
-    if request.method == 'POST':
-        input = request.form
-
-        df = pd.DataFrame(
-            {
-                'gender' : [input['gender']],
-                'age' : [input['age']],
-                'hypertension' : [input['blood pressure']],
-                'heart_disease' : [input['heart disease']],
-                'ever_married' : [input['married']],
-                'work_type' : [input['type of work']],
-                'Residence_type' : [input['residence type']],
-                'avg_glucose_level' : [input['average glucose level']],
-                'bmi' : [input['bmi']],
-                'smoking_status' : [input['smoking status']],
-            }
-        )
-
-        prediksi = model.predict_proba(df)[0][1]
-
-        if prediksi > 0.5:
-            stroke = 'Sorry to say but you are prone to Stroke'
-        else:
-            stroke = 'Congrats, you are healthy'
-
-        return render_template('result.html', data = input, pred = stroke, prob = prediksi)
-
 if __name__ == '__main__':
     
     filename = 'Model Final.sav'
